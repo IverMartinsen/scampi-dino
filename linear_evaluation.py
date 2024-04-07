dc0403e105135cbf56b777b86d0dbae1368c31b3
Iver Martinsen <iver.martinsen@outlook.com>
2024-04-07 14:33:35 +0200

    adapt to k fold evaluation, lots of changes

--- linear_evaluation.py
+++ linear_evaluation.py
@@ -24,7 +24,7 @@ from sklearn.metrics import (
     f1_score,
 )
 from sklearn.linear_model import LogisticRegression
-from sklearn.utils import compute_class_weight
+from sklearn.model_selection import train_test_split
 from lora import LoRA_ViT_timm
 
 
@@ -37,12 +37,11 @@ if __name__ == '__main__':
     parser.add_argument('--arch', default='vit_small', type=str, help='Architecture')
     parser.add_argument('--patch_size', default=16, type=int, help='Patch resolution of the model.')
     parser.add_argument("--checkpoint_key", default="teacher", type=str, help='Key to use in the checkpoint (example: "teacher")')
-    parser.add_argument('--data_path', default='./labelled crops 20', type=str, help='Path to evaluation dataset')
+    parser.add_argument('--data_path', default='/Users/ima029/Desktop/SCAMPI/Repository/data/NO 6407-6-5/labelled imagefolders/imagefolder_20_classes_not_split', type=str, help='Path to evaluation dataset')
     parser.add_argument('--lora_rank', default=None, type=int, help='Rank of LoRA projection matrix')
-    parser.add_argument('--seed', default=1234, type=int, help='Random seed')
-    parser.add_argument('--destination', default='', type=str, help='Destination folder to save results')
-    parser.add_argument('--img_size', default=224, type=int)
-    parser.add_argument('--img_size_pred', default=224, type=int)
+    parser.add_argument('--destination', default='', type=str, help='Destination folder for saving results')
+    parser.add_argument('--img_size', default=224, type=int, help='The size of the images used for training the model')
+    parser.add_argument('--img_size_pred', default=224, type=int, help='The size of the images used for prediction')
     args = parser.parse_args()
 
     print("\n".join("%s: %s" % (k, str(v)) for k, v in sorted(dict(vars(args)).items())))
@@ -57,21 +56,12 @@ if __name__ == '__main__':
         pth_transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
     ])
     
-    dataset_train = datasets.ImageFolder(os.path.join(args.data_path, "train"), transform=transform)
-    dataset_val = datasets.ImageFolder(os.path.join(args.data_path, "val"), transform=transform)
+    ds = datasets.ImageFolder(args.data_path, transform=transform)
     
-    data_loader_train = torch.utils.data.DataLoader(
-        dataset_train,
-        batch_size=args.batch_size,
-    )
-    data_loader_val = torch.utils.data.DataLoader(
-        dataset_val,
-        batch_size=args.batch_size,
-    )
-
-    print(f"Data loaded with {len(dataset_train)} train and {len(dataset_val)} val imgs.")
-
+    data_loader = torch.utils.data.DataLoader(ds, batch_size=args.batch_size, shuffle=False)
+    
     # ============ building network ... ============
+    print("Building network...")
     model = vits.__dict__[args.arch](patch_size=args.patch_size, num_classes=0, img_size=[args.img_size])
     print(f"Model {args.arch} {args.patch_size}x{args.patch_size} built.")
     
@@ -79,112 +69,91 @@ if __name__ == '__main__':
         model = LoRA_ViT_timm(model, r=args.lora_rank)
     utils.load_pretrained_weights(model, args.pretrained_weights, args.checkpoint_key, args.arch, args.patch_size)
     model.eval()
-    
-    # ============ extract features ... ============
-    print("Extracting features for train set...")
-
-    train_features = []
-    train_labels = []
-
-    for samples, labels in data_loader_train:
-        train_features.append(model(samples).detach().numpy())
-        train_labels.append(labels.detach().numpy())
         
-    train_features = np.concatenate(train_features, axis=0)
-    train_labels = np.concatenate(train_labels, axis=0)
-    train_filenames = [os.path.basename(f[0]) for f in dataset_train.imgs]
+    # ============ extract features ... ============
+    print("Extracting features...")
 
-    print("Extracting features for val set...")
-    
-    test_features = []
-    test_labels = []
+    features = []
+    labels = []
 
-    for samples, labels in data_loader_val:
-        test_features.append(model(samples).detach().numpy())
-        test_labels.append(labels.detach().numpy())
-
-    test_features = np.concatenate(test_features, axis=0)
-    test_labels = np.concatenate(test_labels, axis=0)
-    test_filenames = [os.path.basename(f[0]) for f in dataset_val.imgs]
+    for samples, labs in data_loader:
+        features.append(model(samples).detach().numpy())
+        labels.append(labs.detach().numpy())
     
-    
-    for label in np.unique(test_labels):
-        print(f"Class {label} has {np.sum(test_labels == label)} samples in the test set.")
+    features = np.concatenate(features, axis=0)
+    labels = np.concatenate(labels, axis=0)
+    filenames = [os.path.basename(f[0]) for f in ds.imgs]
+    class_names = ds.classes
     
     print("Features are ready!\nStart the classification.")
     
+    for label in np.unique(labels):
+        print(f"Class {label} has {np.sum(labels == label)} samples in the data set.")
+    
     os.makedirs(args.destination, exist_ok=True)
-    
-    #num_classes = np.unique(train_labels).shape[0]
-    #class_names = ["class_" + str(i) for i in range(num_classes)]
-    class_names = dataset_train.classes
-    # table for class wise metrics
-    table = pd.DataFrame()
-    table["class"] = class_names
-    # summary table for overall metrics
+
     summary_table = pd.DataFrame()
+    summary_tables_knn = {k: pd.DataFrame() for k in args.nb_knn}
+    conf_mat_stats = {'preds': [], 'labels': []}
     
-    # ============ logistic regression ... ============
-    log_model = LogisticRegression(
-        max_iter=10000,
-        multi_class="multinomial",
-        class_weight="balanced",
-        random_state=args.seed,
-    )
-    
-    log_model.fit(train_features, train_labels)
-    
-    class_weights = compute_class_weight(
-        "balanced", classes=np.unique(train_labels), y=np.concatenate([train_labels, test_labels])
-    )
-    
-    # compute the log loss on the test set
-    y_pred = log_model.predict(test_features)
+    for seed in range(10):
+        print(f"Evaluating seed {seed}...")
         
-    summary_table.loc["log_model", "log_loss"] = log_loss(test_labels, log_model.predict_proba(test_features))
-    
-    # compute the accuracy and balanced accuracy and mean precision on the test set
-    summary_table.loc["log_model", "balanced_accuracy"] = balanced_accuracy_score(test_labels, y_pred)
-    summary_table.loc["log_model", "accuracy"] = accuracy_score(test_labels, y_pred)
-    summary_table.loc["log_model", "mean_precision"] = precision_score(test_labels, y_pred, average="macro")
-    
-    # add the metrics to the table
-    table["log_model" + "_precision"] = precision_score(test_labels, y_pred, average=None)
-    table["log_model" + "_recall"] = recall_score(test_labels, y_pred, average=None)
-    table["log_model" + "_f1_score"] = f1_score(test_labels, y_pred, average=None)
-    
-    # save confusion matrix
-    cm = confusion_matrix(test_labels, y_pred)
-    cm_display = ConfusionMatrixDisplay(
-        cm, display_labels=class_names
+        train_idx, test_idx = train_test_split(np.arange(len(labels)), test_size=0.2, stratify=labels, random_state=seed)
+        
+        X_train, X_test = features[train_idx], features[test_idx]
+        y_train, y_test = labels[train_idx], labels[test_idx]
+        
+        # ============ logistic regression ... ============
+        log_model = LogisticRegression(
+            max_iter=10000,
+            multi_class="multinomial",
+            class_weight="balanced",
+            random_state=seed,
         )
-    cm_display.plot(xticks_rotation="vertical", colorbar=False)
-    cm_display.figure_.savefig(os.path.join(args.destination, "confusion_matrix_" + "log_model" + ".pdf"), bbox_inches="tight", dpi=300)
+
+        log_model.fit(X_train, y_train)
+        
+        y_pred = log_model.predict(X_test)
+        y_proba = log_model.predict_proba(X_test)
+        
+        conf_mat_stats['preds'].append(y_pred)
+        conf_mat_stats['labels'].append(y_test)
+            
+        summary_table.loc[f"log_model_{seed}", "log_loss"] = log_loss(y_test, y_proba)
+        summary_table.loc[f"log_model_{seed}", "balanced_accuracy"] = balanced_accuracy_score(y_test, y_pred)
+        summary_table.loc[f"log_model_{seed}", "accuracy"] = accuracy_score(y_test, y_pred)
+        summary_table.loc[f"log_model_{seed}", "mean_precision"] = precision_score(y_test, y_pred, average="macro")
+        
+        # ============ k-NN ... ============
+        for k in args.nb_knn:
+            print(f"Evaluating k={k}...")
+            
+            knn = KNeighborsClassifier(n_neighbors=k, p=2)
+            
+            knn.fit(X_train, y_train)
+            
+            y_pred = knn.predict(X_test)
+        
+            # compute the accuracy and balanced accuracy and mean precision on the test set
+            summary_tables_knn[k].loc[f"k={k}_seed={seed}", "balanced_accuracy"] = balanced_accuracy_score(y_test, y_pred)
+            summary_tables_knn[k].loc[f"k={k}_seed={seed}", "accuracy"] = accuracy_score(y_test, y_pred)
+            summary_tables_knn[k].loc[f"k={k}_seed={seed}", "mean_precision"] = precision_score(y_test, y_pred, average="macro")
     
-    # also save predictions for each image
-    preds = pd.DataFrame()
-    preds["filename"] = test_filenames
-    preds["true_label"] = test_labels
-    preds["pred_label"] = y_pred
-    preds.to_csv(os.path.join(args.destination, "predictions_" + "log_model" + ".csv"))
+    # ============ summary ... ============
+    cm = confusion_matrix(np.concatenate(conf_mat_stats['labels']), np.concatenate(conf_mat_stats['preds']))
+    cm_display = ConfusionMatrixDisplay(cm, display_labels=class_names)
+    cm_display.plot(xticks_rotation="vertical", colorbar=False, text_kw={"fontsize": 7})
+    cm_display.figure_.savefig(os.path.join(args.destination, "confusion_matrix_" + "log_model" + ".pdf"), bbox_inches="tight", dpi=300)
 
-    # ============ k-NN ... ============
+    summary_table.loc["mean", :] = summary_table.mean()
+    
     for k in args.nb_knn:
-
-        knn = KNeighborsClassifier(n_neighbors=k, p=2)
-        knn.fit(train_features, train_labels)
-        y_pred = knn.predict(test_features)
-        
-        # compute the accuracy and balanced accuracy and mean precision on the test set
-        summary_table.loc[f"k={k}", "balanced_accuracy"] = balanced_accuracy_score(test_labels, y_pred)
-        summary_table.loc[f"k={k}", "accuracy"] = accuracy_score(test_labels, y_pred)
-        summary_table.loc[f"k={k}", "mean_precision"] = precision_score(test_labels, y_pred, average="macro")
-        
-        # add the metrics to the table
-        table[f"k={k}" + "_precision"] = precision_score(test_labels, y_pred, average=None)
-        table[f"k={k}" + "_recall"] = recall_score(test_labels, y_pred, average=None)
-        table[f"k={k}" + "_f1_score"] = f1_score(test_labels, y_pred, average=None)
-        
-    # save pandas table as csv to same folder as pretrained weights
-    table.to_csv(os.path.join(args.destination, "class_wise_metrics.csv"))
+        summary_table_knn = summary_tables_knn[k]
+        summary_table_knn.loc["mean", :] = summary_table_knn.mean() 
+        summary_table_knn.to_csv(os.path.join(args.destination, f"summary_metrics_knn_{k}.csv"))
+    
     summary_table.to_csv(os.path.join(args.destination, "summary_metrics.csv"))
+
+    print("Summary metrics saved.")
+    
