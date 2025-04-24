09d4d57f96ec20f8befd73c539ed9112282df62b
Iver Martinsen <iver.martinsen@outlook.com>
2025-04-24 17:09:42 +0200

    small adjustments

--- example_usage.py
+++ example_usage.py
@@ -1,15 +1,13 @@
 import torch
-from torchvision import datasets
-from torchvision import transforms as pth_transforms
+from torchvision import datasets, transforms
 from vision_transformer import vit_small, vit_base
 from utils import load_pretrained_weights
 
-
-path_to_weights = "path/to/weights"
 path_to_data = "path/to/data"
-
+path_to_weights = "path/to/weights"
 
 if __name__ == "__main__":
+
     # load the model
     model = vit_small(patch_size=16, num_classes=0, img_size=[224])
 
@@ -18,13 +16,13 @@ if __name__ == "__main__":
     model.eval()
 
     # preprocess the data
-    transform = pth_transforms.Compose([
-        pth_transforms.Resize((256, 256), interpolation=3),
-        pth_transforms.CenterCrop(224),
-        pth_transforms.ToTensor(),
-        pth_transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+    transform = transforms.Compose([
+        transforms.Resize((256, 256), interpolation=3),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
     ])
-
+    
     # load the dataset
     dataset = datasets.ImageFolder(path_to_data, transform=transform)
 
@@ -34,3 +32,5 @@ if __name__ == "__main__":
     for samples, labels in data_loader:
         features = model(samples).detach().numpy()
         labels = labels.detach().numpy()
+
+
